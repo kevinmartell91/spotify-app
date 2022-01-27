a7565275dab7c6da19260ca768ce7ca40af00d26
kevinmartel91 <kevinmartell91@gmail.com>
2022-01-26 19:52:52 -0500

    create routes components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,34 @@ import {
   useLocation,
 } from 'react-router-dom';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import { catchError } from './utils';
 import { GlobalStyle } from './styles';
 import styled from 'styled-components/macro';
+import {
+  Login,
+  Profile,
+  TopArtists,
+  TopTracks,
+  Playlists,
+  Playlist,
+} from './pages';
 
-function ScrollTop() {
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -24,38 +47,46 @@ function ScrollTop() {
 
 function App() {
   const [token, setToken] = useState(void 0);
-  const [profile, setProfile] = useState(void 0);
+
   useEffect(() => {
     setToken(accessToken);
-
-    const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
-      setProfile(data);
-      console.log(data);
-    };
-    catchError(fetchData());
   }, []);
 
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         {!token ? (
-          <a className="App-link" href="http://localhost:8080/login">
-            Login in Spotify
-          </a>
+          <Login />
         ) : (
           <>
-            <button onClick={logout}>Log Out</button>
-
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
+            {/* <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton> */}
+            <Router>
+              <ScrollToTop />
+              <Routes>
+                <Route exact path="/" element={<Profile />}></Route>
+              </Routes>
+              <Routes>
+                <Route
+                  exact
+                  path="/top-artist"
+                  element={<TopArtists />}
+                ></Route>
+              </Routes>
+              <Routes>
+                <Route exact path="/top-tracks" element={<TopTracks />}></Route>
+              </Routes>
+              <Routes>
+                <Route exact path="/playlists" element={<Playlists />}></Route>
+              </Routes>
+              <Routes>
+                <Route
+                  exact
+                  path="/playlists/:id"
+                  element={<Playlist />}
+                ></Route>
+              </Routes>
+            </Router>
           </>
         )}
       </header>
